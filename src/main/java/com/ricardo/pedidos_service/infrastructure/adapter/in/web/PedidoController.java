41dde43270c663777091f3a3f28912a2b369eca2
Ricardo Suarez <ricardo.suarez27@outlook.com>
2025-11-29 00:35:41 -0500

    Se creo el controller para exponer el api

---
+++ src/main/java/com/ricardo/pedidos_service/infrastructure/adapter/in/web/PedidoController.java
@@ -0,0 +1,39 @@
+package com.ricardo.pedidos_service.infrastructure.adapter.in.web;
+
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import lombok.RequiredArgsConstructor;
+
+
+import com.ricardo.pedidos_service.domain.model.ResumenCarga;
+import com.ricardo.pedidos_service.domain.ports.in.CargarPedidosUseCase;
+
+import java.io.IOException; 
+
+@RestController
+@RequestMapping("/pedidos")
+@RequiredArgsConstructor
+public class PedidoController {
+
+
+    private final CargarPedidosUseCase cargarPedidosUseCase;
+
+    @PostMapping(value = "/cargar", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ResumenCarga> cargarPedidos(@RequestParam("file") MultipartFile file) {
+
+        if (file.isEmpty()) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        try {
+            ResumenCarga resumen = cargarPedidosUseCase.procesarArchivo(file.getInputStream());
+            return ResponseEntity.ok(resumen);
+
+        } catch (IOException e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+}
